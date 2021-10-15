c8bbb9a2f9ffd9e65e95768f052e6fbe32f91047
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-10-15 13:40:16 -0400

    added imports and Dense layers to model

--- first_model.py
+++ first_model.py
@@ -1,2 +1,9 @@
+from keras.models import Sequential
 from keras.layers import Dense, Flatten
 import numpy as np
+
+model = Sequential()
+model.add(Flatten(input_shape=()))
+model.add(Dense(, activation='relu'))
+model.add(Dense(, activation='relu'))
+model.add(Dense(, activation=''))

4248f98d9ae7c6e5aeec77c5f2f928efb3798d03
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-10-27 14:01:50 -0400

    created first_model.py and used a placeholder number of conditions

--- first_model.py
+++ first_model.py
@@ -2,8 +2,19 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten
 import numpy as np
 
-model = Sequential()
-model.add(Flatten(input_shape=()))
-model.add(Dense(, activation='relu'))
-model.add(Dense(, activation='relu'))
-model.add(Dense(, activation=''))
+def main():
+  model = Sequential()
+  model.add(Flatten(input_shape=(352, 332, 3)))
+  model.add(Dense(100, activation='relu'))
+  model.add(Dense(20, activation='relu'))
+  model.add(Dense(5, activation='')) #replace with actual number of conditions
+
+  model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['sparse_categorical_accuracy'])
+  model.fit(x_train, y_train, epochs=100, validation_data=(x_test, y_test))
+  model.save('./model')
+
+  scores = model.evaluate(x_test, y_test)
+  print(model.metrics_names[1], scores[1] * 100)
+
+if __name__ = '__main__':
+  main()

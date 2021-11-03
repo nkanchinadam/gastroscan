7b59d754d4bc7cc2da69e36cc08e55e52cb06478
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-11-03 08:12:08 -0400

    addded split function to split_data.py

--- split_data.py
+++ split_data.py
@@ -1,19 +1,27 @@
-ABNORMALITY_FILEPATHS = './filepaths/abnormality_filepaths.py'
-CONDITION_FILEPATHS = './filepaths/condition_filepaths.py'
+import os, random
+
+ABNORMALITY_DATASET = './AbnormalityDataset'
+CONDITION_DATASET = './ConditionDataset'
+
+def split(image_folder):
+  images = []
+  for image in image_folder:
+    images.append(image)
+  random.shuffle(images)
+  return images[:int(0.7 * len(images))], images[int(0.7 * len(images)):]
 
 def main():
   file_num = input('Split Abnormality Dataset: Input 0\nsplit Condition Dataset: Input 1\n')
 
   filepaths = []
   if file_num == '0':
-    filepaths = open(ABNORMALITY_FILEPATHS, 'r').readlines()
-  else if file_num == '1':
-    filepaths = open(CONDITION_FILEPATHS, 'r').readlines()
+    filepaths = open(ABNORMALITY_DATASET, 'r').readlines()
+  elif file_num == '1':
+    filepaths = open(CONDITION_DATASET, 'r').readlines()
   else:
     raise ValueError('Input either a 0 or 1 to indicate which dataset to split')
 
-  for filepath in filepaths:
-    
+
 
 
 if __name__ == "__main__":

acfd56e79786af7c65252578b1581fcec58f61d1
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-11-22 13:28:15 -0500

    wrote code to loop through a given directory and create input and expected output numpy arrays for use by the machine learning model

--- first_model.py
+++ first_model.py
@@ -1,6 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 import numpy as np
+import os
+from PIL import Image
 
 def get_labels(filepath):
   f = open(filepath, 'r')
@@ -12,6 +14,31 @@ def get_labels(filepath):
     labels[count] = line
     count += 1
   return labels
+  
+def get_data(dir):
+  data = []
+  for entry in os.scandir(dir):
+    if entry.is_file():
+      im = Image.open(dir + entry.name)
+      imrgb = im.convert('RGB')
+      data.append(imrgb.getdata())
+    else:
+      data += get_data(dir + entry.name + '/')
+      print('bruh')
+  return data
+
+def get_dataset(dir, labels):
+  x = []
+  y = []
+  count = 0
+  for entry in os.scandir(dir):
+    count += 1
+  for entry in os.scandir(dir):
+    label_data = get_data(dir + entry.name + '/')
+    index = labels[entry.name]
+    x += label_data
+    y += [[1 if index == j else 0 for j in range(count)] for i in range(len(label_data))]
+  return np.array(x), np.array(y)
 
 def main():
   model = Sequential()

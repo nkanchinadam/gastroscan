79923b556ef44c635cbd30725e097524200d8007
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-11-30 10:07:06 -0500

    added code to prepare data for use in the neural network

--- first_model.py
+++ first_model.py
@@ -5,6 +5,9 @@ import os
 from PIL import Image
 import paths
 
+HEIGHT = 100
+WIDTH = 100
+
 def get_labels(filepath):
   f = open(filepath, 'r')
   labels = {}
@@ -16,13 +19,26 @@ def get_labels(filepath):
     count += 1
   return labels
   
+def resized_image(filepath):
+  img = Image.open(filepath)
+  imrgb = img.convert('RGB')
+  img_width, img_height = imrgb.size
+  data = imrgb.getdata()  
+  resized = [[[k for k in data[i * img_width + j]] for j in range(img_width)] for i in range(img_height)]
+
+  num_remove_from_height = img_height - HEIGHT
+  resized = resized[(num_remove_from_height // 2) + (num_remove_from_height % 2) : img_height - (num_remove_from_height // 2)]
+
+  num_remove_from_width = img_width - WIDTH
+  for i in range(len(resized)):
+    resized[i] = resized[i][(num_remove_from_width // 2) + (num_remove_from_width % 2) : img_width - (num_remove_from_width // 2)]
+  return resized
+
 def get_data(dir):
   data = []
   for entry in os.scandir(dir):
     if entry.is_file():
-      im = Image.open(dir + entry.name)
-      imrgb = im.convert('RGB')
-      data.append(imrgb.getdata())
+      data.append(resized_image(dir + entry.name))
     else:
       data += get_data(dir + entry.name + '/')
       print('bruh')
@@ -38,10 +54,30 @@ def get_dataset(dir, labels):
     label_data = get_data(dir + entry.name + '/')
     index = labels[entry.name]
     x += label_data
-    y += [[1 if index == j else 0 for j in range(count)] for i in range(len(label_data))]
+    for i in range(len(label_data)):
+      y.append(index)
   return np.array(x), np.array(y)
 
 def main():
+  to_create = input('Create and Train Abnormality Model: Input 0\nCreate and Train Condition Model: Input 1\n')
+  model_name = input('Model Name: ')
+
+  labels = None
+  x_train = None
+  y_train = None
+  x_test = None
+  y_test = None
+  if to_create == '0':
+    labels = get_labels(paths.ABNORMALITY_LABELS)
+    x_train, y_train = get_dataset(paths.ABNORMALITY_TRAIN, labels)
+    x_test, y_test = get_dataset(paths.ABNORMALITY_TEST, labels)
+  elif to_create == '1':
+    labels = get_labels(paths.CONDITION_LABELS)
+    x_train, y_train = get_dataset(paths.CONDITION_TRAIN, labels)
+    x_test, y_test = get_dataset(paths.CONDITION_TEST, labels)
+  else:
+    raise ValueError('Input either a 0 or 1 to indicate which dataset to create and train a model on')
+
   model = Sequential()
   model.add(Flatten(input_shape=(352, 332, 3)))
   model.add(Dense(100, activation='relu'))

9057f1d04d868efb13253ac52be5169caff548ae
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-12-06 15:35:13 -0500

    reformatted paths.py

--- paths.py
+++ paths.py
@@ -4,11 +4,10 @@ ABNORMALITY_DATASET = './datasets/abnormality_dataset/'
 ABNORMALITY_TRAIN = './datasets/abnormality_train/'
 ABNORMALITY_TEST = './datasets/abnormality_test/'
 ABNORMALITY_LABELS = './labels/abnormality_labels.txt'
+ABNORMALITY_MODELS = './models/abnormalities/'
 
 CONDITION_DATASET = './datasets/condition_dataset/'
 CONDITION_TRAIN = './datasets/condition_train/'
 CONDITION_TEST = './datasets/condition_test/'
 CONDITION_LABELS = './labels/condition_labels.txt'
-
-ABNORMALITY_MODELS = './models/abnormalities/'
 CONDITION_MODELS = './models/conditions/'

3688487c0bc1b5f71bdc21a8cda25d301b298b27
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-12-17 13:28:55 -0500

    got rid of unnecessary resizing code in favor of using Image.resize()

--- data_preparation.py
+++ data_preparation.py
@@ -4,8 +4,6 @@ from PIL import Image
 
 HEIGHT = 100
 WIDTH = 100
-IDEAL_WINDOW_SIZE = (2, 2)
-RESIZE_METHOD = 'crop' # 'crop' or 'resize'
 
 def get_labels(filepath):
   f = open(filepath, 'r')
@@ -17,57 +15,11 @@ def get_labels(filepath):
     labels[count] = line
     count += 1
   return labels
-  
-def crop_image(data):
-  img_height = len(data)
-  img_width = len(data[0])
-
-  num_remove_from_height = img_height - HEIGHT
-  data = data[(num_remove_from_height // 2) + (num_remove_from_height % 2) : img_height - (num_remove_from_height // 2)]
-
-  num_remove_from_width = img_width - WIDTH
-  for i in range(len(data)):
-    data[i] = data[i][(num_remove_from_width // 2) + (num_remove_from_width % 2) : img_width - (num_remove_from_width // 2)]
-
-def average_color(data, top_left_r, top_left_c, height, width):
-  r = 0
-  g = 0
-  b = 0
-  for i in range(top_left_r, top_left_r + height):
-    for j in range(top_left_c, top_left_c + width):
-      r += data[i][j][0]
-      g += data[i][j][1]
-      b += data[i][j][2]
-  return [r // (height * width), g // (height * width), b // (height * width)]
-
-def pool_image(data, window_height, window_width):
-  pooled_data = []
-  for i in range(len(data) - (window_height - 1)):
-    pooled_data.append([])
-    for j in range(len(data[0]) - (window_width - 1)):
-      pooled_data[i].append(average_color(data, i, j, window_height, window_width))
-  return pooled_data
-
-def resize_image(data):
-  while len(data) > HEIGHT or len(data[0]) > WIDTH:
-    window_height = 1 if len(data) == HEIGHT else IDEAL_WINDOW_SIZE[0] if (len(data) - HEIGHT) % (IDEAL_WINDOW_SIZE[0] - 1) == 0 else ((len(data) - HEIGHT) % (IDEAL_WINDOW_SIZE[0] - 1)) + 1
-    window_width = 1 if len(data) == WIDTH else IDEAL_WINDOW_SIZE[1] if (len(data) - WIDTH) % (IDEAL_WINDOW_SIZE[1] - 1) == 0 else ((len(data) - WIDTH) % (IDEAL_WINDOW_SIZE[1] - 1)) + 1
-    data = pool_image(data, window_height, window_width)
-  return data
 
 def load_image(filepath):
   img = Image.open(filepath)
-  imrgb = img.convert('RGB')
-  img_width, img_height = imrgb.size
-  data = imrgb.getdata()  
-  resized = [[[k for k in data[i * img_width + j]] for j in range(img_width)] for i in range(img_height)]
-  if RESIZE_METHOD == 'crop':
-    crop_image(resized)
-  elif RESIZE_METHOD == 'resize':
-    resized = resize_image(resized)
-  else:
-    raise ValueError('Invalid resize method')
-  return resized
+  new_img = img.resize((WIDTH, HEIGHT))
+  return new_img.getdata()
 
 def get_data(dir):
   data = []

e056af38e4a6135171f43a94f2b69db8ba05090d
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-02-02 11:43:32 -0500

    used the combined dataset instead of the separate training and testing sets for training the GAN

--- gan.py
+++ gan.py
@@ -24,26 +24,22 @@ def get_discriminator_loss(real_predictions, fake_predictions):
 def main():
   to_load = input('Create Abnormality GAN: Input 0\nCreate Condition GAN: Input 1\n')
 
-  x_train = None
-  x_test = None
-  y_train = None
-  y_test = None
+  x = None
+  y = None
   if to_load == '0':
     labels = dp.get_labels(paths.ABNORMALITY_LABELS)
-    x_train, y_train = dp.get_dataset(paths.ABNORMALITY_TRAIN, labels)
-    x_test, y_test = dp.get_dataset(paths.ABNORMALITY_TEST, labels)
+    x, y = dp.get_dataset(paths.ABNORMALITY_DATASET, labels)
   elif to_load == '1':
     labels = dp.get_labels(paths.CONDITION_LABELS)
-    x_train, y_train = dp.get_dataset(paths.CONDITION_TRAIN, labels)
-    x_test, y_test = dp.get_dataset(paths.CONDITION_TEST, labels)
+    x, y = dp.get_dataset(paths.CONDITION_DATASET, labels)
   else:
     raise ValueError('Input either a 0 or 1 to indicate which dataset to create and train a GAN on')
 
-  x_train = (x_train - 127.5) / 127.5
-  x_test = (x_test - 127.5) / 127.5
+  x = (x - 127.5) / 127.5
 
-  BUFFER_SIZE = x_train.shape[0]
+  BUFFER_SIZE = x.shape[0]
   BATCH_SIZE = 30
+  x = tf.data.Dataset.from_tensor_slices(x).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
   discriminator = make_discriminator_model()
   discriminator_optimizer = tf.optimizers.Adam(1e-3)

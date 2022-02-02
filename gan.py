3d6b2761ec778c63e2ec18f4ed6f4767a6fbc30a
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-02-02 00:39:08 -0500

    added code to normalize data and use BUFFER_SIZE and BATCH_SIZE; also added necessary imports to do so

--- gan.py
+++ gan.py
@@ -1,5 +1,9 @@
 import data_preparation as dp
 import paths
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 def main():
   to_load = input('Create Abnormality GAN: Input 0\nCreate Condition GAN: Input 1\n')
@@ -18,6 +22,14 @@ def main():
     x_test, y_test = dp.get_dataset(paths.CONDITION_TEST, labels)
   else:
     raise ValueError('Input either a 0 or 1 to indicate which dataset to create and train a GAN on')
+
+  x_train = (x_train - 127.5) / 127.5
+  x_test = (x_test - 127.5) / 127.5
+
+  BUFFER_SIZE = x_train.shape[0]
+  BATCH_SIZE = 30
+  x_train = tf.data.Dataset.from_tensor_slices(x_train).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+  x_test = tf.data.Dataset.from_tensor_slices(x_test).batch(BATCH_SIZE)
 
 if __name__ == '__main__':
   main()

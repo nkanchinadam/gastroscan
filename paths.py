ef023ff564d9d5d899e0694c61daf8b9f65b1ea8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-04-25 15:35:06 -0400

    combined ABNORMALITY_GANS and CONDITION_GANS paths into one combined GANS path

--- paths.py
+++ paths.py
@@ -5,11 +5,11 @@ ABNORMALITY_TRAIN = './datasets/abnormality_train/'
 ABNORMALITY_TEST = './datasets/abnormality_test/'
 ABNORMALITY_LABELS = './labels/abnormality_labels.txt'
 ABNORMALITY_MODELS = './models/classifiers/abnormalities/'
-ABNORMALITY_GANS = './models/gans/abnormalities/'
 
 CONDITION_DATASET = './datasets/condition_dataset/'
 CONDITION_TRAIN = './datasets/condition_train/'
 CONDITION_TEST = './datasets/condition_test/'
 CONDITION_LABELS = './labels/condition_labels.txt'
 CONDITION_MODELS = './models/classifiers/conditions/'
-CONDITION_GANS = './models/gans/conditions/'
+
+GANS = './models/gans/'
